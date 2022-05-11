887160450499fafb0f847dc9b77579597de48123
KaramEisa <karam.eisa21@gmail.com>
2022-05-11 13:02:02 +0300

    added fetch API

---
+++ Assets/Scripts/index.js
@@ -0,0 +1,36 @@
+
+//! JS – Bootcamp capsules
+
+//? 1. Test the api calls with postman
+//? 2. Create an editable table with the data you are getting.
+//? 3. Create a button that can remove the row from your UI.
+//? 4. Create a button that can edit your row. Once you click
+
+const body = document.querySelector('body');
+const container = document.querySelector('.container');
+
+// const getArrOfPeople = async () => {
+  
+//     const res = fetch('https://capsules-asb6.herokuapp.com/api/teacher/mordi');
+//     console.log(res);
+// };
+
+fetch('https://capsules-asb6.herokuapp.com/api/teacher/mordi').then((res) => {
+    return res.json();
+    
+}).then((data) => {
+
+
+})
+
+
+
+
+export const createGameBoard = (row, column) => {
+  let gameTable = table(arryOfCards, row, column);
+  gameTable = gameTable + "</table>";
+  document.getElementById("flashcard").innerHTML = gameTable;
+  assignIds();
+  const gameBoard = document.querySelector(".table");
+  gameBoard.addEventListener("click", addClickEvent);
+}
